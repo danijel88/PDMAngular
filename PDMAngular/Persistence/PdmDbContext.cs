9e9107c36615bd3c2f749824d20403636f14ee9c
Danijel Boksan <danijel.boksan@live.com>
2017-11-21 16:20:02 +0100

    Add DbContext

---
+++ PDMAngular/Persistence/PdmDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PDMAngular.Persistence
+{
+    public class PdmDbContext : DbContext
+    {
+        public PdmDbContext(DbContextOptions<PdmDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
