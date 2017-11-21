9f15302d5398e65b2a21d200ca963ba258d8ae78
Danijel Boksan <danijel.boksan@live.com>
2017-11-21 16:23:01 +0100

    Create Migrations for ItemTypes and MachineTypes

--- PDMAngular/Persistence/PdmDbContext.cs
+++ PDMAngular/Persistence/PdmDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PDMAngular.Models;
 
 namespace PDMAngular.Persistence
 {
     public class PdmDbContext : DbContext
     {
+        public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<MachineType> MachineTypes { get; set; }
+
         public PdmDbContext(DbContextOptions<PdmDbContext> options) : base(options)
         {
 
