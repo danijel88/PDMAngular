aae614a5f35447e4d2c013d4c16ea6cb6f04b094
Danijel Boksan <danijel.boksan@live.com>
2017-11-21 16:12:58 +0100

    Add  service for db context

--- PDMAngular/Startup.cs
+++ PDMAngular/Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PDMAngular.Persistence;
 
 namespace PDMAngular
 {
@@ -22,6 +20,11 @@ namespace PDMAngular
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PdmDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             services.AddMvc();
         }
 
