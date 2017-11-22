73f23e954763f0af17864b1b9f3a06bce7c4925e
Danijel Boksan <danijelboksan@gmail.com>
2017-11-22 13:16:25 +0100

    Create Resources

---
+++ PDMAngular/Controllers/Resources/MachineTypeResource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PDMAngular.Controllers.Resources
+{
+    public class MachineTypeResource
+    {
+        public int Id { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
