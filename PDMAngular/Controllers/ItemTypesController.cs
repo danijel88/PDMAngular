9ce36e852cddd93c3ebdcbfbe5a4e36d4a00eee9
Danijel Boksan <danijelboksan@gmail.com>
2017-11-22 15:47:30 +0100

    ItemTypeController get all item types

---
+++ PDMAngular/Controllers/ItemTypesController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PDMAngular.Controllers.Resources;
+using PDMAngular.Models;
+using PDMAngular.Persistence;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PDMAngular.Controllers
+{
+    [Route("/api/itemtypes")]
+    public class ItemTypesController : Controller
+    {
+        private readonly PdmDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ItemTypesController(PdmDbContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ItemTypeResource>> GetItemTypes()
+        {
+            var itemTypes = await _context.ItemTypes.ToListAsync();
+            return _mapper.Map<List<ItemType>, List<ItemTypeResource>>(itemTypes);
+        }
+    }
+}
