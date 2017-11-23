e01b4609c88e914293d4310faeeace85d675d4f1
Danijel Boksan <danijelboksan@gmail.com>
2017-11-23 14:32:02 +0100

    Create Item DomainModel

---
+++ PDMAngular/Models/Item.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PDMAngular.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string InternalCode { get; set; }
+
+        public string Description { get; set; }
+
+        public int? Band { get; set; }
+
+        public int? Enter { get; set; }
+
+        public int? Exit { get; set; }
+
+        public int? Thickness { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string MadeBy { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string Color { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public string UserId { get; set; }
+
+        public ItemType ItemType { get; set; }
+
+        public int ItemTypeId { get; set; }
+
+        public MachineType MachineType { get; set; }
+
+        public int MachineTypeId { get; set; }
+
+
+
+
+    }
+}
