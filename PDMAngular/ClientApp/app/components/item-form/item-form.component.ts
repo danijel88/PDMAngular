b289abacd6af145260e333d8f4314928bc994d93
Danijel Boksan <danijelboksan@gmail.com>
2017-11-23 14:32:36 +0100

    Creating form for Item to add in database

---
+++ PDMAngular/ClientApp/app/components/item-form/item-form.component.ts
@@ -0,0 +1,31 @@
+﻿
+import { Component, OnInit } from '@angular/core';
+import { ItemTypeService } from '../../services/itemtype.service';
+import { MachineTypeService } from '../../services/machinetype.service';
+
+@Component({
+    selector: 'item-form',
+    templateUrl: './item-form.component.html'
+})
+export class ItemFormComponent implements OnInit {
+    itemTypes: any;
+    machineTypes: any;
+    item= {};
+
+    constructor(private itemTypeService: ItemTypeService,
+        private mahcineTypeService: MachineTypeService) {
+        
+    }
+
+    ngOnInit() {
+        this.itemTypeService.getItemTypes().subscribe(it => {
+            this.itemTypes = it;
+        });
+
+        this.mahcineTypeService.getMachineTypes().subscribe(mt => {
+            this.machineTypes = mt;
+        });
+    }
+
+
+}
