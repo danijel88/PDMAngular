540a3279268b8c3592496653accc6c930d2d7e4a
Danijel Boksan <danijelboksan@gmail.com>
2017-11-23 14:32:14 +0100

    Create ItemResource for using in API

---
+++ PDMAngular/Controllers/Resources/ItemResource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PDMAngular.Controllers.Resources
+{
+    public class ItemResource
+    {
+        public int Id { get; set; }
+
+        public string InternalCode { get; set; }
+
+        public string Description { get; set; }
+
+        public int? Band { get; set; }
+
+        public int? Enter { get; set; }
+
+        public int? Exit { get; set; }
+
+        public int? Thickness { get; set; }
+
+        public string MadeBy { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string Color { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public string UserId { get; set; }
+
+
+        public int ItemTypeId { get; set; }
+
+
+        public int MachineTypeId { get; set; }
+    }
+}
