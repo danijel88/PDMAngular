0d3b708207fda32838fd998b92adb23d90d84b36
Danijel Boksan <danijelboksan@gmail.com>
2017-11-23 14:32:54 +0100

    create form for inserting ItemType record in database

---
+++ PDMAngular/ClientApp/app/components/itemtype-form/itemtype-form.component.ts
@@ -0,0 +1,23 @@
+﻿
+import { Component, OnInit } from '@angular/core';
+import { ItemTypeService } from "../../services/itemtype.service";
+
+@Component({
+    selector: 'itemtype-form',
+    templateUrl: 'itemtype-form.component.html'
+})
+export class ItemTypeFormComponent implements OnInit {
+
+    itemTypes: any;
+    itemType = {};
+    constructor(private itemTypeService: ItemTypeService) {
+    }
+
+    ngOnInit() {
+        this.itemTypeService.getItemTypes().subscribe(it => {
+            this.itemTypes = it;
+            console.log("ITEM TYPES", this.itemTypes);
+        });
+        
+    }
+}
