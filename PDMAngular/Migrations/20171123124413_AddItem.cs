1d78eb492853048ff8ba0db45239f2d180c61ddb
Danijel Boksan <danijelboksan@gmail.com>
2017-11-23 14:31:33 +0100

    creating migration for Item

---
+++ PDMAngular/Migrations/20171123124413_AddItem.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PDMAngular.Migrations
+{
+    public partial class AddItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Band = table.Column<int>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Enter = table.Column<int>(nullable: true),
+                    Exit = table.Column<int>(nullable: true),
+                    InternalCode = table.Column<string>(maxLength: 50, nullable: false),
+                    ItemTypeId = table.Column<int>(nullable: false),
+                    MachineTypeId = table.Column<int>(nullable: false),
+                    MadeBy = table.Column<string>(maxLength: 150, nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Thickness = table.Column<int>(nullable: true),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_ItemTypes_ItemTypeId",
+                        column: x => x.ItemTypeId,
+                        principalTable: "ItemTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Items_MachineTypes_MachineTypeId",
+                        column: x => x.MachineTypeId,
+                        principalTable: "MachineTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ItemTypeId",
+                table: "Items",
+                column: "ItemTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_MachineTypeId",
+                table: "Items",
+                column: "MachineTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
