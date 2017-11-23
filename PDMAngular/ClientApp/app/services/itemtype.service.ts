61ae4a003e7c4367ca6d35b7fef4e43f25216a20
Danijel Boksan <danijelboksan@gmail.com>
2017-11-23 14:30:56 +0100

    get data from backend API

---
+++ PDMAngular/ClientApp/app/services/itemtype.service.ts
@@ -0,0 +1,16 @@
+﻿
+import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ItemTypeService {
+
+    constructor(private http: Http) { }
+
+    getItemTypes() {
+        return this.http
+            .get('/api/itemtypes/')
+            .map(res => res.json());
+    }
+}
---
+++ PDMAngular/ClientApp/app/services/machinetype.service.ts
@@ -0,0 +1,16 @@
+﻿import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class MachineTypeService {
+
+    constructor(private http: Http){}
+
+    getMachineTypes() {
+        return this.http
+            .get('/api/machinetypes/')
+            .map(res => res.json());
+    }
+
+}
