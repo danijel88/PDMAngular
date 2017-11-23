ac2a15d667e2607880c836627ede4cf64d005b4e
Danijel Boksan <danijelboksan@gmail.com>
2017-11-23 14:31:18 +0100

    Create Controller for MachineTypes and create method to get all machine types

---
+++ PDMAngular/Controllers/MachineTypesController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PDMAngular.Controllers.Resources;
+using PDMAngular.Models;
+using PDMAngular.Persistence;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PDMAngular.Controllers
+{
+    [Route("/api/machinetypes")]
+    public class MachineTypesController : Controller
+    {
+        private readonly PdmDbContext _context;
+        private readonly IMapper _mapper;
+
+        public MachineTypesController(PdmDbContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<MachineTypeResource>> GetMachineTypes()
+        {
+            var machineTypes = await _context.MachineTypes.ToListAsync();
+
+            return _mapper.Map<List<MachineType>, List<MachineTypeResource>>(machineTypes);
+        }
+    }
+}
