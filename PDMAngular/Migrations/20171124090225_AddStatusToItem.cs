5606e7b9aa096d74b325e9ac47d9d880b45435d7
Danijel Boksan <danijelboksan@gmail.com>
2017-11-24 14:13:02 +0100

    create migration from elastic and status

---
+++ PDMAngular/Migrations/20171124090225_AddStatusToItem.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PDMAngular.Migrations
+{
+    public partial class AddStatusToItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Items");
+        }
+    }
+}
---
+++ PDMAngular/Migrations/20171124124028_AddElasticToItem.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PDMAngular.Migrations
+{
+    public partial class AddElasticToItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Elastic",
+                table: "Items",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Elastic",
+                table: "Items");
+        }
+    }
+}
