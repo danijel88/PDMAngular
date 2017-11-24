0baa944e8eb17cba069c92a4d145808630a308ce
Danijel Boksan <danijelboksan@gmail.com>
2017-11-24 14:13:11 +0100

    ItemController

---
+++ PDMAngular/Controllers/ItemsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDMAngular.Models;
+
+namespace PDMAngular.Controllers
+{
+    [Route("/api/items")]
+    public class ItemsController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreateItem([FromBody]Item item)
+        {
+            return Ok(item);
+        }
+    }
+}
