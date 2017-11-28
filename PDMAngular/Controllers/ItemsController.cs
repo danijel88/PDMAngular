33c4dbc623be15e82d049bcc03e07cbc37fcb0a5
Danijel Boksan <danijelboksan@gmail.com>
2017-11-28 14:38:29 +0100

    Creating new Item and Update exist one.

--- PDMAngular/Controllers/ItemsController.cs
+++ PDMAngular/Controllers/ItemsController.cs
@@ -1,15 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PDMAngular.Controllers.Resources;
 using PDMAngular.Models;
+using PDMAngular.Persistence;
+using System;
+using System.Threading.Tasks;
 
 namespace PDMAngular.Controllers
 {
     [Route("/api/items")]
     public class ItemsController : Controller
     {
-        [HttpPost]
-        public IActionResult CreateItem([FromBody]Item item)
+        private readonly IMapper _mapper;
+        private readonly PdmDbContext _context;
+
+        public ItemsController(IMapper mapper, PdmDbContext context)
         {
-            return Ok(item);
+            _mapper = mapper;
+            _context = context;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItem([FromBody]ItemResource itemResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var item = _mapper.Map<ItemResource, Item>(itemResource);
+            item.CreateDate = DateTime.Now;
+
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            var result = _mapper.Map<Item, ItemResource>(item);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, [FromBody]ItemResource itemResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var item = await _context.Items.FindAsync(id);
+            _mapper.Map<ItemResource, Item>(itemResource, item);
+
+            item.UpdateDate = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            var result = _mapper.Map<Item, ItemResource>(item);
+
+            return Ok(result);
+        }
+
     }
 }
