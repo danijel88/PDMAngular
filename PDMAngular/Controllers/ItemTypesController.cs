1eeea15685447b4c8bbd618105caa112afd683cf
Danijel Boksan <danijelboksan@gmail.com>
2017-11-28 14:38:52 +0100

    Create new Item and Machine Type, also update exist one

--- PDMAngular/Controllers/ItemTypesController.cs
+++ PDMAngular/Controllers/ItemTypesController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using PDMAngular.Controllers.Resources;
 using PDMAngular.Models;
 using PDMAngular.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -22,6 +23,39 @@ namespace PDMAngular.Controllers
             _mapper = mapper;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateItemType([FromBody] ItemTypeResource itemTypeResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var itemType = _mapper.Map<ItemTypeResource, ItemType>(itemTypeResource);
+            itemType.CreateDate = DateTime.Now;
+
+            _context.ItemTypes.Add(itemType);
+            await _context.SaveChangesAsync();
+
+            var result = _mapper.Map<ItemType, ItemTypeResource>(itemType);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItemType(int id, [FromBody] ItemTypeResource itemTypeResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var itemType = await _context.ItemTypes.FindAsync(id);
+            _mapper.Map<ItemTypeResource, ItemType>(itemTypeResource);
+            itemType.UpdateDate = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            var result = _mapper.Map<ItemType, ItemTypeResource>(itemType);
+            return Ok(result);
+        }
+
+        [HttpGet]
         public async Task<IEnumerable<ItemTypeResource>> GetItemTypes()
         {
             var itemTypes = await _context.ItemTypes.ToListAsync();
--- PDMAngular/Controllers/MachineTypesController.cs
+++ PDMAngular/Controllers/MachineTypesController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using PDMAngular.Controllers.Resources;
 using PDMAngular.Models;
 using PDMAngular.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -21,6 +22,42 @@ namespace PDMAngular.Controllers
             _context = context;
             _mapper = mapper;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMachineType([FromBody] MachineTypeResource machineTypeResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var machineType = _mapper.Map<MachineTypeResource, MachineType>(machineTypeResource);
+
+            machineType.CreateDate = DateTime.Now;
+
+            _context.MachineTypes.Add(machineType);
+            await _context.SaveChangesAsync();
+
+            var result = _mapper.Map<MachineType, MachineTypeResource>(machineType);
+            return Ok(result);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCreateMachineType(int id, [FromBody] MachineTypeResource machineTypeResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var machineType = await _context.MachineTypes.FindAsync(id);
+            _mapper.Map<MachineTypeResource, MachineType>(machineTypeResource);
+
+            machineType.UpdateDate = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            var result = _mapper.Map<MachineType, MachineTypeResource>(machineType);
+            return Ok(result);
+        }
+
 
         [HttpGet]
         public async Task<IEnumerable<MachineTypeResource>> GetMachineTypes()
