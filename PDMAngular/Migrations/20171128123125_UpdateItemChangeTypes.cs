28c7fb8e2803f352af8e96302006fff76dcbbfdc
Danijel Boksan <danijelboksan@gmail.com>
2017-11-28 14:41:03 +0100

    Migration for ItemUpdate

---
+++ PDMAngular/Migrations/20171128123125_UpdateItemChangeTypes.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PDMAngular.Migrations
+{
+    public partial class UpdateItemChangeTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Thickness",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Exit",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Enter",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Band",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Thickness",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Exit",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Enter",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Band",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+        }
+    }
+}
