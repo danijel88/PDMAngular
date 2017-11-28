26283428d5a84081a0ed292a27c8043085150dfc
Danijel Boksan <danijelboksan@gmail.com>
2017-11-28 14:39:29 +0100

    Update on correct way ItemResources. Add some DataAnnotations

--- PDMAngular/Controllers/Resources/ItemResource.cs
+++ PDMAngular/Controllers/Resources/ItemResource.cs
@@ -1,5 +1,5 @@
 ﻿using PDMAngular.Models;
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PDMAngular.Controllers.Resources
 {
@@ -7,35 +7,33 @@ namespace PDMAngular.Controllers.Resources
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string InternalCode { get; set; }
 
         public string Description { get; set; }
 
-        public int? Band { get; set; }
+        public decimal? Band { get; set; }
 
-        public int? Enter { get; set; }
+        public decimal? Enter { get; set; }
 
-        public int? Exit { get; set; }
+        public decimal? Exit { get; set; }
 
-        public int? Thickness { get; set; }
+        public decimal? Thickness { get; set; }
 
         public bool Elastic { get; set; }
 
+        [Required]
+        [StringLength(150)]
         public string MadeBy { get; set; }
-
-        public DateTime CreateDate { get; set; }
 
         public string Color { get; set; }
 
         public string Name { get; set; }
 
-        public DateTime? UpdateDate { get; set; }
-
         public string UserId { get; set; }
 
-
         public int ItemTypeId { get; set; }
-
 
         public int MachineTypeId { get; set; }
 
