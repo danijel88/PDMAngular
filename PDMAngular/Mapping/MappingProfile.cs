752b156c48887d628635d00343a3241280691aa6
Danijel Boksan <danijelboksan@gmail.com>
2017-11-28 14:40:27 +0100

    Update mapping Domain to API and reversed.

--- PDMAngular/Mapping/MappingProfile.cs
+++ PDMAngular/Mapping/MappingProfile.cs
@@ -8,8 +8,18 @@ namespace PDMAngular.Mapping
     {
         public MappingProfile()
         {
+            //Domain to API Resource
             CreateMap<ItemType, ItemTypeResource>();
             CreateMap<MachineType, MachineTypeResource>();
+            CreateMap<Item, ItemResource>();
+
+
+            //API Resource to Domain
+            CreateMap<ItemResource, Item>()
+                .ForMember(i => i.Id, opt => opt.Ignore());
+            CreateMap<ItemTypeResource, ItemType>();
+            CreateMap<MachineTypeResource, MachineType>();
+
         }
     }
 }
