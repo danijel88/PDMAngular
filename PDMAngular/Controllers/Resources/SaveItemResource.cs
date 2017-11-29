73af265eadda90d220d24c793692964ac18aa111
Danijel Boksan <danijelboksan@gmail.com>
2017-11-29 15:38:34 +0100

    Add new resource for saving item

---
+++ PDMAngular/Controllers/Resources/SaveItemResource.cs
@@ -0,0 +1,43 @@
+﻿using PDMAngular.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace PDMAngular.Controllers.Resources
+{
+    public class SaveItemResource
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string InternalCode { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal? Band { get; set; }
+
+        public decimal? Enter { get; set; }
+
+        public decimal? Exit { get; set; }
+
+        public decimal? Thickness { get; set; }
+
+        public bool Elastic { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string MadeBy { get; set; }
+
+        public string Color { get; set; }
+
+        public string Name { get; set; }
+
+        public string UserId { get; set; }
+
+        public int ItemTypeId { get; set; }
+
+        public int MachineTypeId { get; set; }
+
+        public Status Status { get; set; }
+
+    }
+}
