a0c2d24034fbb85931a4822c8056e39113b0f35e
Danijel Boksan <danijelboksan@gmail.com>
2017-12-04 15:16:06 +0100

    no message

--- PDMAngular/Startup.cs
+++ PDMAngular/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PDMAngular.Core;
 using PDMAngular.Persistence;
 
 namespace PDMAngular
