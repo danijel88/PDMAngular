a7999c916af2b1a25f18a28870b9f7ef4d2b947f
Danijel Boksan <danijelboksan@gmail.com>
2017-12-05 12:21:57 +0100

    Add new API Resource for ItemHist

---
+++ PDMAngular/Controllers/Resources/SaveItemHistResource.cs
@@ -0,0 +1,36 @@
+﻿using PDMAngular.Core.Models;
+
+namespace PDMAngular.Controllers.Resources
+{
+    public class SaveItemHistResource
+    {
+
+        public string InternalCode { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal? Band { get; set; }
+
+        public decimal? Enter { get; set; }
+
+        public decimal? Exit { get; set; }
+
+        public decimal? Thickness { get; set; }
+
+        public bool Elastic { get; set; }
+
+        public string MadeBy { get; set; }
+
+        public string Color { get; set; }
+
+        public string Name { get; set; }
+
+        public string UserId { get; set; }
+
+        public int ItemTypeId { get; set; }
+
+        public int MachineTypeId { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
