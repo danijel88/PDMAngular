43c3b9e41ae5a1c5a253aa7493cc957ecc307ac3
Danijel Boksan <danijelboksan@gmail.com>
2017-12-06 14:32:29 +0100

    Update ItemService with new functions for filtering

--- PDMAngular/ClientApp/app/services/item.service.ts
+++ PDMAngular/ClientApp/app/services/item.service.ts
@@ -25,4 +25,39 @@ export class ItemService {
             .map(res => res.json());
     }
 
+    deleteItem(id)
+    {
+        return this.http.delete('/api/items/' + id)
+            .map(res => res.json());
+    }
+
+    //Client side filtering
+    //getItems()
+    //{
+    //    return this.http.get('/api/items')
+    //        .map(res => res.json());
+    //}
+
+    //server side filtering
+    getItems(filter)
+    {
+   
+        return this.http.get('/api/items' + '?' + this.toQueryString(filter))
+            .map(res => res.json());
+    } 
+
+    toQueryString(obj) {
+        var parts: any[] = [];
+        
+        for (var property in obj)
+        {
+            var value = obj[property];
+            console.log(value);
+            if (value != null && value != 'undefined')
+                   parts.push(encodeURIComponent(property) + '=' + encodeURIComponent(value));
+            
+        }
+        return parts.join('&');
+    }
+
 }
